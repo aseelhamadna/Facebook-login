cefdd095a32079987e4e7c77d4216c89b9e14e9e
aseelhamadna <aseelhamadna@gmail.com>
2020-03-15 16:22:38 +0200

    FB login

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { Facebook, FacebookLoginResponse } from '@ionic-native/facebook/ngx';
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -7,6 +7,14 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor(private fb: Facebook) {}
+  login(){
+    this.fb.login(['public_profile', 'user_friends', 'email'])
+  .then((res: FacebookLoginResponse) => 
+  // console.log('Logged into Facebook!', res)
+  alert(JSON.stringify(res))
+  )
+  .catch(e => console.log('Error logging into Facebook', e));
 
+  }
 }
